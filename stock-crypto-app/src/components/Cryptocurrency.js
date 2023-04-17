c3ce8b6ce3ab42610a6f4734466c5d993be041eb
maxdickinson2 <max17dickinson@hotmail.com>
2023-04-17 07:26:48 -0700

    fixes

--- stock-crypto-app/src/components/Cryptocurrency.js
+++ stock-crypto-app/src/components/Cryptocurrency.js
@@ -1,81 +1,80 @@
-import React from 'react';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Cryptocurrency.css';
 import useAuth from '../hooks/useAuth';
 
-const FavoriteButton = ({ currency }) => {
-  const { token } = useAuth();
+function Cryptocurrency({ currency }) {
+  const { user, token } = useAuth();
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(() => {
+    if (user && user.favorites.includes(currency.id)) {
+      setIsFavorite(true);
+    } else {
+      setIsFavorite(false);
+    }
+  }, [user, currency.id]);
 
   const handleAddToFavorites = async () => {
-    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    const userId = JSON.parse(localStorage.getItem('user')).id; 
-
     try {
-      await axios.put(
-        `https://stark-chamber-73716.herokuapp.com/user/${userId}/favorites`,
-        { ...currency, timestamp: new Date().getTime() },
-        { headers: { Authorization: `Bearer ${token}` } }
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/favorites`,
+        { currencyId: currency.id },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
       );
-      console.log('Added to favorites:', currency.name);
-      localStorage.setItem('favorites', JSON.stringify([...favorites, currency]));
+      console.log(response.data);
+      setIsFavorite(true);
     } catch (error) {
-      console.error('Error adding to favorites:', error.message);
+      console.error(error);
+    }
+  };
+
+  const handleRemoveFromFavorites = async () => {
+    try {
+      const response = await axios.delete(
+        `${process.env.REACT_APP_API_URL}/favorites/${currency.id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(response.data);
+      setIsFavorite(false);
+    } catch (error) {
+      console.error(error);
     }
   };
 
   return (
-    <button onClick={handleAddToFavorites}>Add to Favorites</button>
-  );
-};
-
-const Cryptocurrency = () => {
-  const [cryptocurrencies, setCryptocurrencies] = useState([]);
-
-  useEffect(() => {
-    const fetchCryptocurrencies = async () => {
-      const vs_currency = 'usd';
-
-      const options = {
-        method: 'GET',
-        url: 'https://api.coingecko.com/api/v3/coins/markets',
-        params: { vs_currency },
-      };
-
-      try {
-        const response = await axios.request(options);
-        setCryptocurrencies(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchCryptocurrencies();
-  }, []);
-
-  return (
-    <div className="coin-list">
-      <h1 className="title">Cryptocurrencies</h1>
-      <div className="coin-grid">
-        {cryptocurrencies.map((currency) => (
-          <div key={currency.id} className="coin-card">
-            <div className="coin-card-image">
-              <img src={currency.image} alt={`${currency.name} logo`} />
-            </div>
-            <div className="coin-card-details">
-              <h2 className="coin-name">{currency.name}</h2>
-              <p className="coin-symbol">{currency.symbol.toUpperCase()}</p>
-              <p className="coin-price">${currency.current_price.toLocaleString()}</p>
-              <FavoriteButton currency={currency} />
-            </div>
-          </div>
-        ))}
+    <div className="cryptocurrency">
+      <div className="cryptocurrency-name">
+        {currency.name} ({currency.symbol.toUpperCase()})
       </div>
+      <div className="cryptocurrency-price">${currency.price_usd}</div>
+      {user ? (
+        <div className="cryptocurrency-favorite">
+          {isFavorite ? (
+            <button onClick={handleRemoveFromFavorites}>Remove from Favorites</button>
+          ) : (
+            <button onClick={handleAddToFavorites}>Add to Favorites</button>
+          )}
+        </div>
+      ) : (
+        <div className="cryptocurrency-login">
+          Please <a href="/login">log in</a> to add to favorites.
+        </div>
+      )}
     </div>
   );
-};
+}
 
 export default Cryptocurrency;
+
 
 
 
