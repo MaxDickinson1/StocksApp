5f2518688ed06b43540f558bd80e494d6d1c6d8b
maxdickinson2 <max17dickinson@hotmail.com>
2023-04-17 08:09:10 -0700

    trytin to deploy

--- stock-crypto-app/src/components/Homepage.js
+++ stock-crypto-app/src/components/Homepage.js
@@ -1,51 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './Cryptocurrency.css';
 
-const HomePage = ({ userId }) => {
+const Homepage = () => {
   const [favorites, setFavorites] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const fetchFavorites = async () => {
-      const token = localStorage.getItem('token');
-      const favorites = user && user.favorites ? user.favorites : [];
-      console.log('userIdFromStorage:', userIdFromStorage);
-      try {
-        const response = await axios.get(
-          `https://stark-chamber-73716.herokuapp.com/user/${userIdFromStorage}/favorites`, 
-          { headers: { Authorization: `Bearer ${token}` } }
-        );
-        console.log('Favorites fetched:', response.data);
-        setFavorites(response.data);
-      } catch (error) {
-        console.error('Error fetching favorites:', error.message);
-      }
-    };
-  
-    if (localStorage.getItem('token')) {
-      fetchFavorites();
+    const userIdFromStorage = localStorage.getItem('userId');
+    if (userIdFromStorage) {
+      axios
+        .get(`https://stark-chamber-73716.herokuapp.com/user/${userIdFromStorage}/favorites`)
+        .then((response) => {
+          setFavorites(response.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error('Error fetching favorites:', error.message);
+          setLoading(false);
+        });
+    } else {
+      setLoading(false);
     }
   }, []);
-  
 
   return (
-    <div className="coin-list">
-      <h1 className="title">Favorites</h1>
-      <div className="coin-grid">
-        {favorites.map((favorite) => (
-          <div key={`${favorite.type}-${favorite.symbol}`} className="coin-card">
-            <div className="coin-card-details">
-              <h2 className="coin-name">{favorite.name}</h2>
-              <p className="coin-symbol">{favorite.symbol.toUpperCase()}</p>
-            </div>
-          </div>
-        ))}
-      </div>
+    <div>
+      <h1>My Favorites</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : favorites.length ? (
+        <ul>
+          {favorites.map((favorite) => (
+            <li key={favorite.id}>{favorite.title}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No favorites yet.</p>
+      )}
     </div>
   );
 };
 
-export default HomePage;
+export default Homepage;
+
 
 
 
