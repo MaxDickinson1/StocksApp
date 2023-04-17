eef12c63527644d4cac7963dbcec031c97ffc2d5
maxdickinson2 <max17dickinson@hotmail.com>
2023-04-17 02:40:16 -0700

    new favorites page

--- stock-crypto-app/src/components/Homepage.js
+++ stock-crypto-app/src/components/Homepage.js
@@ -1,49 +1,64 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { Card, Row, Col } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 
-const Homepage = () => {
-  const [favorites, setFavorites] = useState([]);
+const AddFavorites = ({ userId, token, onSuccess }) => {
+  const [symbol, setSymbol] = useState('');
+  const [type, setType] = useState('stock');
+  const [name, setName] = useState('');
 
-  useEffect(() => {
-    const fetchFavorites = async () => {
-      const userId = localStorage.getItem('userId');
-      const token = localStorage.getItem('token');
-
-      if (!userId || !token) return;
-
-      try {
-        const response = await axios.get(`https://stark-chamber-73716.herokuapp.com/users/${userId}`, {
-          headers: { Authorization: `Bearer ${token}` },
-        });
-        setFavorites(response.data.favorites);
-      } catch (error) {
-        console.error(error);
-      }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const payload = {
+      type,
+      symbol,
+      name,
     };
 
-    fetchFavorites();
-  }, []);
+    try {
+      await axios.put(`https://stark-chamber-73716.herokuapp.com/users/${userId}/favorites`, payload, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setSymbol('');
+      setType('stock');
+      setName('');
+
+      onSuccess();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
-      <h1>Your Favorites</h1>
-      <Row>
-        {favorites.map((favorite, index) => (
-          <Col key={index} md={4}>
-            <Card>
-              <Card.Body>
-                <Card.Title>{favorite.name} ({favorite.symbol})</Card.Title>
-                <Card.Text>Price: {favorite.price}</Card.Text>
-                <Card.Text>Change: {favorite.change}</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      <h3>Add Favorite</h3>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>Type:</Form.Label>
+          <Form.Control as="select" value={type} onChange={(e) => setType(e.target.value)}>
+            <option value="stock">Stock</option>
+            <option value="crypto">Cryptocurrency</option>
+          </Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Symbol:</Form.Label>
+          <Form.Control type="text" value={symbol} onChange={(e) => setSymbol(e.target.value)} />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Name:</Form.Label>
+          <Form.Control type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Add
+        </Button>
+      </Form>
     </div>
   );
 };
 
-export default Homepage;
+export default AddFavorites;
+
 
