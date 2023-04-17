a3123cd6bd547b842d893a3bec6741cf708759e1
maxdickinson2 <max17dickinson@hotmail.com>
2023-04-17 07:17:58 -0700

    bug fixes

--- stock-crypto-app/src/components/Cryptocurrency.js
+++ stock-crypto-app/src/components/Cryptocurrency.js
@@ -1,11 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
 import './Cryptocurrency.css';
-import useAuth from './useAuth';
+import useAuth from '../hooks/useAuth';
+
+const FavoriteButton = ({ currency }) => {
+  const { token } = useAuth();
+
+  const handleAddToFavorites = async () => {
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    const userId = JSON.parse(localStorage.getItem('user')).id; 
+
+    try {
+      await axios.put(
+        `https://stark-chamber-73716.herokuapp.com/user/${userId}/favorites`,
+        { ...currency, timestamp: new Date().getTime() },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      console.log('Added to favorites:', currency.name);
+      localStorage.setItem('favorites', JSON.stringify([...favorites, currency]));
+    } catch (error) {
+      console.error('Error adding to favorites:', error.message);
+    }
+  };
+
+  return (
+    <button onClick={handleAddToFavorites}>Add to Favorites</button>
+  );
+};
 
 const Cryptocurrency = () => {
   const [cryptocurrencies, setCryptocurrencies] = useState([]);
-  const { token } = useAuth();
 
   useEffect(() => {
     const fetchCryptocurrencies = async () => {
@@ -28,25 +52,6 @@ const Cryptocurrency = () => {
     fetchCryptocurrencies();
   }, []);
 
-  const handleAddToFavorites = async (currency) => {
-    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    const { user } = useAuth();
-  
-    try {
-      await axios.put(
-        `https://stark-chamber-73716.herokuapp.com/user/${user.id}/favorites`,
-        { ...currency, timestamp: new Date().getTime() },
-        { headers: { Authorization: `Bearer ${token}` } }
-      );
-      console.log('Added to favorites:', currency.name);
-      localStorage.setItem('favorites', JSON.stringify([...favorites, currency]));
-    } catch (error) {
-      console.error('Error adding to favorites:', error.message);
-    }
-  };
-  
-  
-
   return (
     <div className="coin-list">
       <h1 className="title">Cryptocurrencies</h1>
@@ -60,7 +65,7 @@ const Cryptocurrency = () => {
               <h2 className="coin-name">{currency.name}</h2>
               <p className="coin-symbol">{currency.symbol.toUpperCase()}</p>
               <p className="coin-price">${currency.current_price.toLocaleString()}</p>
-              <button onClick={() => handleAddToFavorites(currency)}>Add to Favorites</button>
+              <FavoriteButton currency={currency} />
             </div>
           </div>
         ))}
@@ -70,6 +75,7 @@ const Cryptocurrency = () => {
 };
 
 export default Cryptocurrency;
+
 
 
 
