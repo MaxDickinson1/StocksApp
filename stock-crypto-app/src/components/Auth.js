4cb46f78eadfc291b17a68e3a8e82cd71453a077
maxdickinson2 <max17dickinson@hotmail.com>
2023-04-17 08:57:49 -0700

    logged

--- stock-crypto-app/src/components/Auth.js
+++ stock-crypto-app/src/components/Auth.js
@@ -1,18 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import './Cryptocurrency.css';
+import UserContext from './UserContext';
 
 const Auth = () => {
+  const { setUser } = useContext(UserContext);
   const [isLogin, setIsLogin] = useState(true);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-    }
-  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -21,55 +16,43 @@ const Auth = () => {
 
     try {
       const response = await axios.post(apiUrl, { username, password });
-      console.log(response.data);
-      localStorage.setItem('token', response.data.token);
-      axios.defaults.headers.common.Authorization = `Bearer ${response.data.token}`;
+      const { data: { user, token } } = response;
+
+
+      localStorage.setItem('user', JSON.stringify(user));
+      localStorage.setItem('token', token);
+
+      setUser(user);
     } catch (error) {
       console.error(error);
     }
   };
 
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    axios.defaults.headers.common.Authorization = null;
-  };
-
   return (
     <div className="auth-container">
-      {localStorage.getItem('token') ? (
-        <div>
-          <h1 className="auth-title">You are logged in!</h1>
-          <button className="auth-button" onClick={handleLogout}>
-            Log out
-          </button>
-        </div>
-      ) : (
-        <>
-          <h1 className="auth-title">{isLogin ? 'Login' : 'Register'}</h1>
-          <form className="auth-form" onSubmit={handleSubmit}>
-            <input
-              className="auth-input"
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-              className="auth-input"
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button className="auth-button" type="submit">
-              {isLogin ? 'Login' : 'Register'}
-            </button>
-          </form>
-          <p className="auth-switch" onClick={() => setIsLogin(!isLogin)}>
-            {isLogin ? 'No account? Register.' : 'Already have an account? Login.'}
-          </p>
-        </>
-      )}
+      <h1 className="auth-title">{isLogin ? 'Login' : 'Register'}</h1>
+      <form className="auth-form" onSubmit={handleSubmit}>
+        <input
+          className="auth-input"
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          className="auth-input"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="auth-button" type="submit">
+          {isLogin ? 'Login' : 'Register'}
+        </button>
+      </form>
+      <p className="auth-switch" onClick={() => setIsLogin(!isLogin)}>
+        {isLogin ? 'No account? Register.' : 'Already have an account? Login.'}
+      </p>
     </div>
   );
 };
