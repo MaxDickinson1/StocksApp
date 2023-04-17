768b0fc89b4f343cc18d291e95898e55288296e2
maxdickinson2 <max17dickinson@hotmail.com>
2023-04-17 09:39:23 -0700

    no auth

--- stock-crypto-app/src/components/Auth.js
+++ stock-crypto-app/src/components/Auth.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './Auth.css';
+import './Cryptocurrency.css';
 
 const Auth = () => {
   const [isLogin, setIsLogin] = useState(true);
