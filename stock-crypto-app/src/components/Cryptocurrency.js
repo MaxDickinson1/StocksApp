50f1d587001b1025429d65d82d942c9c54770ec3
maxdickinson2 <max17dickinson@hotmail.com>
2023-04-17 09:25:32 -0700

    description

--- stock-crypto-app/src/components/Cryptocurrency.js
+++ stock-crypto-app/src/components/Cryptocurrency.js
@@ -1,57 +1,114 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, useRef } from 'react';
+import { useParams } from 'react-router-dom';
+import Chart from 'chart.js/auto';
+import 'chartjs-adapter-date-fns';
+import { coinGeckoClient } from './api';
 import './Cryptocurrency.css';
 
-const Cryptocurrency = () => {
-  const [cryptocurrencies, setCryptocurrencies] = useState([]);
+const CoinDetail = () => {
+  const [coin, setCoin] = useState(null);
+  const [prices, setPrices] = useState([]);
+  const chartRef = useRef(null);
+  const { id } = useParams();
 
   useEffect(() => {
-    const fetchCryptocurrencies = async () => {
-      const vs_currency = 'usd'; 
-
+    const fetchCoin = async () => {
+      const thirtyDaysAgo = new Date();
+      thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
       const options = {
         method: 'GET',
-        url: 'https://api.coingecko.com/api/v3/coins/markets',
-        params: { vs_currency },
+        url: `/coins/${id}/market_chart/range`,
+        params: {
+          vs_currency: 'usd',
+          from: Math.floor(thirtyDaysAgo.getTime() / 1000),
+          to: Math.floor(Date.now() / 1000),
+        },
       };
 
       try {
-        const response = await axios.request(options);
-        setCryptocurrencies(response.data);
+        const response = await coinGeckoClient.request(options);
+        setCoin(response.data);
+        setPrices(response.data.prices);
       } catch (error) {
         console.error(error);
       }
     };
 
-    fetchCryptocurrencies();
-  }, []);
+    fetchCoin();
+  }, [id]);
+
+  useEffect(() => {
+    if (coin && chartRef.current) {
+      const context = chartRef.current.getContext('2d');
+
+      if (context) {
+        const labels = prices.map((price) => {
+          return new Date(price[0]);
+        });
+
+        if (chartRef.current.chart) {
+          chartRef.current.chart.destroy();
+        }
+
+        const newChart = new Chart(context, {
+          type: 'line',
+          data: {
+            labels,
+            datasets: [
+              {
+                label: `${coin.name} Price`,
+                data: prices.map((price) => price[1]),
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1,
+              },
+            ],
+          },
+          options: {
+            scales: {
+              x: {
+                type: 'time',
+                time: {
+                  unit: 'day',
+                },
+              },
+              y: {
+                ticks: {
+                  callback: (value, index, values) => {
+                    return '$' + value.toLocaleString();
+                  },
+                },
+              },
+            },
+          },
+        });
+
+        chartRef.current.chart = newChart;
+      }
+    }
+  }, [coin, prices, chartRef]);
 
   return (
-    <div className="coin-list">
-      <h1 className="title">Cryptocurrencies</h1>
-      <div className="coin-grid">
-        {cryptocurrencies.map((currency) => (
-          <Link key={currency.id} to={`/coins/${currency.id}`} className="coin-card">
-            <div className="coin-card-image">
-              <img src={currency.image} alt={`${currency.name} logo`} />
+    <div className="coin-detail">
+      {coin && (
+        <div className="coin-detail-content">
+          <div className="coin-detail-chart">
+            <canvas ref={chartRef}></canvas>
+          </div>
+          {coin.description && coin.description.en && (
+            <div className="coin-detail-description">
+              <h2>Description</h2>
+              <p>{coin.description.en}</p>
             </div>
-            <div className="coin-card-details">
-              <h2 className="coin-name">{currency.name}</h2>
-              <p className="coin-symbol">{currency.symbol.toUpperCase()}</p>
-              <p className="coin-price">${currency.current_price.toLocaleString()}</p>
-              {currency.description && currency.description.en && (
-                <p className="coin-description">{currency.description.en}</p>
-              )}
-            </div>
-          </Link>
-        ))}
-      </div>
+          )}
+        </div>
+      )}
     </div>
   );
 };
 
-export default Cryptocurrency;
+export default CoinDetail;
+
 
 
 
