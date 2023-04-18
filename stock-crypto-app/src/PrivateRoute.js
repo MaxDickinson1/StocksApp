a996f5f5647434df586dffd5be6e60577fd0b624
maxdickinson2 <max17dickinson@hotmail.com>
2023-04-18 16:43:57 -0700

    took away a .

--- stock-crypto-app/src/PrivateRoute.js
+++ stock-crypto-app/src/PrivateRoute.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Navigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
+import { useAuth } from './contexts/AuthContext';
+
 
 const PrivateRoute = ({ element, ...rest }) => {
   const { currentUser } = useAuth();
