94bf938fe7414bcee213064d69bfd3dc97ba097e
maxdickinson2 <max17dickinson@hotmail.com>
2023-04-18 17:34:58 -0700

    usehistory

--- stock-crypto-app/src/PrivateRoute.js
+++ stock-crypto-app/src/PrivateRoute.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
+import { Route, useHistory } from 'react-router-dom';
 import { useAuth } from './contexts/AuthContext';
-
 
 const PrivateRoute = ({ element, ...rest }) => {
   const { currentUser } = useAuth();
+  const history = useHistory();
 
-  return (
-    <Route
-      {...rest}
-      element={
-        currentUser ? (
-          element
-        ) : (
-          <Navigate to="/auth" replace />
-        )
-      }
-    />
-  );
+  if (!currentUser) {
+    history.replace('/auth');
+    return null;
+  }
+
+  return <Route {...rest} element={element} />;
 };
 
 export default PrivateRoute;
